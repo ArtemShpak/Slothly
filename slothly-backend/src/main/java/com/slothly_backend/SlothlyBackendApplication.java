d6a358e6c8c08cc2c4dcccecab887c174b2a1851
Artem Shpak <artem6491@gmail.com>
2024-12-03 17:50:35 +0200

    Added README file and backend template

---
+++ slothly-backend/src/main/java/com/slothly_backend/SlothlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.slothly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlothlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlothlyBackendApplication.class, args);
+	}
+
+}
---
+++ slothly-backend/src/test/java/com/slothly_backend/SlothlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slothly_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlothlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
