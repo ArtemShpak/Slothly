c3a9ea8593a914a5ce7f760c269111a24008b22e
Artem Shpak <artem6491@gmail.com>
2024-12-17 00:53:57 +0200

    Added a registration

--- slothly-frontend/src/app/app-routing.module.ts
+++ slothly-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './authentication/log-in/log-in.component';
 import {HelloWorldComponent} from './hello-world/hello-world.component';
+import {SignUpComponent} from './authentication/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'hello-world', component: HelloWorldComponent},
+  {path: 'sign-up', component: SignUpComponent},
 ];
 
 @NgModule({
--- slothly-frontend/src/app/authentication/sign-up/sign-up.service.ts
+++ slothly-frontend/src/app/authentication/sign-up/sign-up.service.ts
@@ -8,7 +8,7 @@ export class SignUpService {
 
   constructor(private http: HttpClient) { }
 
-  private apiUrl = 'http://localhost:8080/sign-up';
+  private apiUrl = 'http://localhost:8080/api/v1/register';
 
   PostData(user: any) {
     return this.http.post(this.apiUrl, user, { responseType: 'text' });
